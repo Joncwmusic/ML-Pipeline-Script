3faacdd231a223db916c5af13b962ae125a5d9d0
JonCWMusic <jon.chris.whelpley@gmail.com>
2024-01-26 16:42:44 -0500

    Added placeholder files for sequence forecasting and prediction and moved cleaning functions to separate file. Also adding tests for cleaning file

---
+++ CleaningTests.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import CleaningFunctions
+
+
+# Genrate data with missing values, Categorical Variables, and crazy outliers
---
+++ SequenceModels.py
@@ -0,0 +1,5 @@
+### Seasonality Trend Separation
+### Recurrent Neural Networks (LSTM, GRU)
+### ARIMA Models
+### Exponential Smoothing
+### Simple Moving Averages
